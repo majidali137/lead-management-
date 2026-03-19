2029bad42f46f59288c91f0c619b546e27216bd0
majidali137 <majidaliwp69@gmail.com>
2026-03-19 18:01:40 +0500

    connect backend with frontend

---
+++ frontend/utils/api.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+export const getLeads = async () => {
+  const res = await API.get("/leads");
+  return res.data.data; // 👈 direct array return
+};
+
+export const createLead = async (data: {
+  name: string;
+  email: string;
+  status: string;
+}) => {
+  const res = await API.post("/leads", data);
+  return res.data;
+};
